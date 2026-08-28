12c306258c2bf73e54630243c6a48fd65d7f40ad
MechaDragonX <theofficialrv@gmail.com>
2026-08-28 15:26:22 -0700

    Remove commented code

--- bchunkGUI/bchunkgui.py
+++ bchunkGUI/bchunkgui.py
@@ -110,34 +110,12 @@ def file_dialog_handler(filetype):
     
 
 def bin_dialog_handler():
-    # global bin_path
-    # global output_dir
-    # global output_name
-    # bin_path = file_dialog(filetype=0)
-    # dpg.set_value('bin_label', f'".bin" file{os.path.basename(bin_path)}')
-    # output_dir = f'{os.path.dirname(bin_path)}/'
-    # dpg.delete_item(outfile_tag)
-    # output_name = os.path.basename(bin_path)[:-4]
-    # dpg.add_input_text(label=outfile_input_label, tag=outfile_tag, hint=output_name, parent=outfile_input_group)
-    # dpg.set_value('outfile_label', f'Output file: {output_name}')
     file_dialog_handler(0)
 
 def cue_dialog_handler():
-    # global cue_path
-    # global output_dir
-    # global output_name
-    # cue_path = file_dialog(filetype=1)
-    # dpg.set_value('cue_label', f'".cue" file{os.path.basename(cue_path)}')
-    # output_dir = f'{os.path.dirname(cue_path)}/'
-    # dpg.delete_item(outfile_tag)
-    # output_name = os.path.basename(cue_path)[:-4]
-    # dpg.add_input_text(label=outfile_input_label, tag=outfile_tag, hint=output_name, parent=outfile_input_group)
-    # dpg.set_value('outfile_label', f'Output file: {output_name}')
     file_dialog_handler(1)
 
 def output_dialog_callback():
-    # output_name = dpg.get_value('outfile_input')
-    # dpg.set_value('outfile_label', output_name)
     global output_dir
     output_dir = get_path_with_dialog(filetype=2)
 
