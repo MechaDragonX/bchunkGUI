4300ccba6fce2a56f01dd59b5363201ed2e64e87
MechaDragonX <theofficialrv@gmail.com>
2026-08-28 15:14:40 -0700

    Add the ability to pick an output dir and output filename

--- bchunkGUI/bchunkgui.py
+++ bchunkGUI/bchunkgui.py
@@ -7,33 +7,54 @@ import crossfiledialog as cfd
 
 bin_path = ''
 cue_path = ''
+output_dir = ''
+output_name = ''
+outfile_tag  = 'outfile_input'
+outfile_input_label = 'Output filename WITHOUT ".ISO"'
+outfile_input_group = 'outfile_input_group'
+parent_window = 'primary'
 
 
 '''
 filetypes:
 0: bin
 1: cue
+2: directory
 '''
-def file_dialog(filetype):
+def get_path_with_dialog(filetype):
     path = '[Not Selected]'
+
+    start = ''
+    if (0 and 1) or output_dir == '':
+        start = os.getcwd()
+    else:
+        start = os.path.dirname(output_dir)
 
     if filetype == 0:
         result = cfd.open_file(
             title='Select ".bin" file',
-            start_dir=os.getcwd(),
+            start_dir=start,
             filter={'".bin" file': '*.bin'}
         )
-    else:
+    elif filetype == 1:
         result = cfd.open_file(
             title='Select ".cue" file',
-            start_dir=os.getcwd(),
+            start_dir=start,
             filter={'".cue" file': '*.cue'}
+        )
+    else:
+        result = cfd.choose_folder(
+            title='Select output folder',
+            start_dir=start
         )
 
     if result:
         path = result
 
-    return path
+    if filetype != 2:
+        return path
+    else:
+        return output_dir
 
 
 def run_bchunk():
@@ -67,24 +88,71 @@ def run_bchunk():
 dpg.create_context()
 
 
-def bin_dialog():
-    global bin_path
-    bin_path = file_dialog(filetype=0)
-    dpg.set_value('bin_label', f'bin: {os.path.basename(bin_path)}')
+def file_dialog_handler(filetype):
+    global output_dir
+    global output_name
 
+    if filetype == 0:
+        global bin_path
+        bin_path = get_path_with_dialog(filetype=0)
+        output_dir = f'{os.path.dirname(bin_path)}/'
+        dpg.set_value('bin_label', f'bin: {os.path.basename(bin_path)}')
+        output_name = os.path.basename(bin_path)[:-4]
+    else:
+        global cue_path
+        cue_path = get_path_with_dialog(filetype=1)
+        dpg.set_value('cue_label', f'cue: {os.path.basename(cue_path)}')
+        output_name = os.path.basename(cue_path)[:-4]
 
-def cue_dialog():
-    global cue_path
-    cue_path = file_dialog(filetype=1)
-    dpg.set_value('cue_label', f'cue: {os.path.basename(cue_path)}')
+    dpg.delete_item(outfile_tag)
+    dpg.add_input_text(label=outfile_input_label, tag=outfile_tag, hint=output_name, parent=outfile_input_group)
+    dpg.set_value('outfile_label', f'Output filename: {output_name}')
+    
+
+def bin_dialog_handler():
+    # global bin_path
+    # global output_dir
+    # global output_name
+    # bin_path = file_dialog(filetype=0)
+    # dpg.set_value('bin_label', f'bin: {os.path.basename(bin_path)}')
+    # output_dir = f'{os.path.dirname(bin_path)}/'
+    # dpg.delete_item(outfile_tag)
+    # output_name = os.path.basename(bin_path)[:-4]
+    # dpg.add_input_text(label=outfile_input_label, tag=outfile_tag, hint=output_name, parent=outfile_input_group)
+    # dpg.set_value('outfile_label', f'Output file: {output_name}')
+    file_dialog_handler(0)
+
+def cue_dialog_handler():
+    # global cue_path
+    # global output_dir
+    # global output_name
+    # cue_path = file_dialog(filetype=1)
+    # dpg.set_value('cue_label', f'cue: {os.path.basename(cue_path)}')
+    # output_dir = f'{os.path.dirname(cue_path)}/'
+    # dpg.delete_item(outfile_tag)
+    # output_name = os.path.basename(cue_path)[:-4]
+    # dpg.add_input_text(label=outfile_input_label, tag=outfile_tag, hint=output_name, parent=outfile_input_group)
+    # dpg.set_value('outfile_label', f'Output file: {output_name}')
+    file_dialog_handler(1)
+
+def output_dialog_callback():
+    # output_name = dpg.get_value('outfile_input')
+    # dpg.set_value('outfile_label', output_name)
+    global output_dir
+    output_dir = get_path_with_dialog(filetype=2)
 
 
 with dpg.window(tag='primary'):
-    dpg.add_button(label='Open ".bin" file', callback=bin_dialog)
+    dpg.add_button(label='Open ".bin" file', callback=bin_dialog_handler)
     dpg.add_text('bin: [Not Selected]', tag='bin_label')
 
-    dpg.add_button(label='Open ".cue" file', callback=cue_dialog)
+    dpg.add_button(label='Open ".cue" file', callback=cue_dialog_handler)
     dpg.add_text('cue: [Not Selected]', tag='cue_label')
+
+    dpg.add_button(label='Select output folder', callback=output_dialog_callback)
+    with dpg.group(tag='outfile_input_group'):
+        dpg.add_input_text(label='Output filename WITHOUT ".ISO"', tag='outfile_input')
+    dpg.add_text('Output filename: [Not Set]', tag='outfile_label')
 
     dpg.add_button(label='Convert to ".iso" file', callback=run_bchunk)
     dpg.add_text('', tag='progress_label')
